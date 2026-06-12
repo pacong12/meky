c4fe33abd5eb23f876d2cb8cc5d77ce034a83246
pacong12 <mutingudin12@gmail.com>
2026-06-12 10:32:45 +0700

    fix: downgrade wagmi to v2, remove serverExternalPackages, add .npmrc

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  serverExternalPackages: [
-    "@rainbow-me/rainbowkit",
-    "wagmi",
-    "viem",
-    "@wagmi/connectors",
-    "@walletconnect/ethereum-provider",
-    "@walletconnect/universal-provider",
-    "@safe-global/safe-apps-sdk",
-    "@safe-global/safe-apps-provider",
-    "@reown/appkit",
-    "@reown/appkit-ui",
-    "@reown/appkit-scaffold-ui",
-    "ox",
-  ],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
