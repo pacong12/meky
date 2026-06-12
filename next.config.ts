26498204d39f3d41069845153e3ccd4e91b5f506
pacong12 <mutingudin12@gmail.com>
2026-06-12 10:11:04 +0700

    chore: add server external package configurations and include @swc/helpers dependency

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "@rainbow-me/rainbowkit",
+    "wagmi",
+    "viem",
+    "@wagmi/connectors",
+    "@walletconnect/ethereum-provider",
+    "@walletconnect/universal-provider",
+    "@safe-global/safe-apps-sdk",
+    "@safe-global/safe-apps-provider",
+    "@reown/appkit",
+    "@reown/appkit-ui",
+    "@reown/appkit-scaffold-ui",
+    "ox",
+  ],
 };
 
 export default nextConfig;
